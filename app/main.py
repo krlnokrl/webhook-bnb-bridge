933e57f86b6700ea1b5a42d61ad77a6ba194f293
krlnokrl <80167428+krlnokrl@users.noreply.github.com>
2022-05-30 11:28:47 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from web3.auto import w3
 
 app = Flask(__name__)
 
+account_1 = '0xFa37214c234dF3479246AB5a850a11d591A93240'
+
 @app.route("/")
 def home_view():
 	return "<h1>Hello World!</h1>"
@@ -12,3 +14,18 @@ def home_view():
 @app.route('/test_address/<string:address>', methods=['GET'])
 def test_address(address):
 	return w3.isChecksumAddress(address)
+	
+@app.route('/bet_bear')
+def bet_bear():
+
+	nonce = web3.eth.getTransactionCount(account_1)
+	
+	tx = {
+    'nonce': nonce,
+    'to': '0x00',
+    'value': web3.toWei(1, 'ether'),
+    'gas': 2000000,
+    'gasPrice': web3.toWei('50', 'gwei')
+	}
+	
+	return json.dumps(tx)
