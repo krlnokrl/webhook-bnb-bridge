57fd5046dfc6181cce7b39e32e1f5897e04fbad0
krlnokrl <80167428+krlnokrl@users.noreply.github.com>
2022-05-30 11:24:20 +0200

    test-web3

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from web3.auto import w3
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-        return "<h1>Hello World!</h1>"
+	return "<h1>Hello World!</h1>"
+
+	
+@app.route('/test_address/<string:address>', methods=['GET'])
+def test_address(address):
+	return w3.isChecksumAddress(address)
