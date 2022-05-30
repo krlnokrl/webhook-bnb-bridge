02775246c3e3ea6a3958fcb5d508b27c6954261f
krlnokrl <80167428+krlnokrl@users.noreply.github.com>
2022-05-30 11:04:58 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,16 +1,9 @@
 from flask import Flask, json
 
-from web3.auto import w3
 
 app = Flask(__name__)
 
-@app.route('/getTest/<string:address>', methods=['GET'])
-def deposit_address(address):
-	result = w3.isChecksumAddress(address)
-	return result
-	
+
 @app.route('/')
 def index():
   return 'Testing...'
-if __name__ == '__main__':
-	api.run()
